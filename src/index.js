771e7d02efaca54fc167a81c54c61b2564cde012
你的用户名$ git config --global user.name 你的用户名^V <80231115+Smnatic95@users.noreply.github.com>
2021-03-16 20:38:45 +0800

    day0

--- src/index.js
+++ src/index.js
@@ -1,17 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+function Clock(props) {
+  return(
+          <div>
+            <h1>
+              现在的时间是{props.date.toLocaleTimeString()}
+            </h1>
+            <h2>这是函数组件</h2>
+          </div>
+  )
+}
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+function run() {
+  ReactDOM.render(
+    <Clock date={new Date()} />
+    ,document.querySelector("#root")
+  )
+}
+
+setInterval(run, 1000);
 reportWebVitals();
